3f0a74531691ff8a4ec8119aa214e8f3750b69ca
Callum Hogsden <orbinas@gmail.com>
2022-11-30 13:54:22 +1100

    Update tags.py

--- ext/tags.py
+++ ext/tags.py
@@ -1,36 +1,64 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import app_commands
 
-import json
+from typing import Optional
 
+import json
+from difflib import get_close_matches
+ 
 class Tags(commands.Cog):
+
+    tags = {}
 
     def __init__(self, bot):
         self.bot = bot
+        self.check_tags.start()
+
+    async def cog_unload(self):
+        self.check_tags.cancel()
 
     @app_commands.command()
-    async def tags(self, interaction: discord.Interaction):
-        """ List current tags (mini commands that return static text) """
-        with open("ext/data/tags.json") as f:
-            tags = json.load(f)
+    @app_commands.describe(tag="The tag you want the bot to post")
+    async def tags(self, interaction: discord.Interaction, tag: Optional[str]):
+        """ Posts text matching a given tag, no arguments lists all tags"""
 
-        await interaction.response.send_message(
-            ", ".join(sorted(tags.keys())),
-            ephemeral=True)
+        if not tag:
+            await interaction.response.send_message(
+                ", ".join(sorted(self.tags.keys())), ephemeral=True)
+        else:
+            await interaction.response.send_message(self.tags[tag])
+
+
+    @tags.autocomplete('tag')
+    async def tag_autocomplete(self, interaction: discord.Interaction,
+        current: str,) -> list[app_commands.Choice[str]]:
+
+        return [app_commands.Choice(name=tag, value=tag)
+            for tag in self.tags.keys() if current.lower() in tag.lower()]
 
 
     @commands.Cog.listener("on_message")
     async def scan_tags(self, message):
 
         if self.bot.user in message.mentions:
-            with open("ext/data/tags.json") as f:
-                tags = json.load(f)
             try:
-                await message.channel.send(tags[message.content.split()[1]])
+                await message.channel.send(
+                    self.tags[message.content.split()[1]])
             except:
                 pass
 
 
+    @tasks.loop(seconds=60.0)
+    async def check_tags(self):
+
+        with open("ext/data/tags.json") as f:
+            tags = json.load(f)
+        self.tags = tags
+
+
 async def setup(bot):
     await bot.add_cog(Tags(bot))
+
+async def teardown(bot):
+    await bot.remove_cog(Tags)
